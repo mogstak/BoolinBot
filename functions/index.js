0992e12d87f85b9c662dc63b7d640522cb372c61
Alissa <alissayarmantho1@gmail.com>
2021-01-08 19:06:11 +0800

    Added emoji to text
    Haven't edit the emoji dictionary

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,33 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 var request = require('request');
+var emoji = require("@jukben/emoji-search");
 
 // our single entry point for every message
 app.post('/', async (req, res) => {
   let curUserId = req.body.message.chat.id;
   let chatContent = req.body.message.text;
-
+  function main(txt) {
+    spliced_txt = txt.split(" ");
+    var stringArray = new Array();
+    var mssg = "";
+    for (var i = 0; i < spliced_txt.length; i++) {
+      stringArray.push(spliced_txt[i]);
+      const search_result = emoji.default(spliced_txt[i]);
+      if (search_result.length != 0) {
+        for (var j = 0; j < search_result.length; j++) {
+          stringArray.push(search_result[j].char);
+        }
+      }
+      else {
+        stringArray.push(" ");
+      }
+    }
+    for (var i = 0; i < stringArray.length; i++) {
+      mssg = mssg + stringArray[i];
+    }
+      return mssg;
+  }
   if (curUserId) {
       let requestUrl = 'https://api.telegram.org/bot' + process.env.BOT_TOKEN + '/sendMessage';
 
@@ -17,14 +38,15 @@ app.post('/', async (req, res) => {
         return res.status(200).send();
       });
 
-      function sendMessage(curUserId, chatContent, callback) {
+    function sendMessage(curUserId, chatContent, callback) {
+        const return_message = main(chatContent);
         request.post({
           url: requestUrl,
           'Content-Type': "application/json;charset=utf-8",
           json: true,
           body: {
             chat_id: curUserId,
-            text: chatContent
+            text: return_message
           }
         }, function (error, result, body) {
             if (error) {
